97e1f9bb81720fc5fdf5f3b4902d2b3266e1a6f0
jYAMall <e.martsinenko@gmail.com>
2022-06-05 00:08:05 +0300

    Body.jsx

--- my_port/src/components/Body.jsx
+++ my_port/src/components/Body.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import videoSite from '../src/components/video.mp4'
+import videoSite from '../video1.mp4'
 
 const Body =  () => {
   return (
     <div className='w-full h-[90vh] top-[90px]'>
-        <video scr={videoSite} autoPlay loop muted />
+        <video src={videoSite} autoPlay loop muted/>
     </div>
   )
 }
